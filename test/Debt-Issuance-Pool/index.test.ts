5d233ee3383d120b2dc0a1c7b61357a8cfb64c06
PunkUnknown <PunkUnknown@tutanota.com>
2021-01-16 13:15:06 +0500

    Basic tests added

--- test/Debt-Issuance-Pool/index.test.ts
+++ test/Debt-Issuance-Pool/index.test.ts
@@ -41,24 +41,24 @@ describe('Debt Issuance Pool', () => {
 
 		const debase = '0x9248c485b0B80f76DA451f167A8db30F33C70907';
 		const dai = '0x6b175474e89094c44da98b954eedeac495271d0f';
-		const policy = '0x989edd2e87b1706ab25b2e8d9d9480de3cc383ed';
+		const policy = '0x989Edd2e87B1706AB25b2E8d9D9480DE3Cc383eD';
 		const burnPool1 = '0xF4168cc431e9a8310e595dB9F7E2564cC96F5D51';
 		const burnPool2 = '0xf5cB771023706Ca566eA6128b88e03A262737479';
 
 		const epochs = 0;
-		const oraclePeriod = 0;
+		const oraclePeriod = 10;
 		const curveShifter = 0;
-		const mean = '';
-		const deviation = '';
-		const oneDivDeviationSqrtTwoPi = '';
-		const twoDeviationSquare = '';
+		const mean = '0x00000000000000000000000000000000';
+		const deviation = '0x3fff0000000000000000000000000000';
+		const oneDivDeviationSqrtTwoPi = '0x40353af632a67dd1dd8e38e38e38e38e';
+		const twoDeviationSquare = '0x40000000000000000000000000000000';
 
 		before(async function() {
 			address = await accounts[0].getAddress();
 			burnPool = await burnPoolFactory.deploy();
 			oracle = await oracleFactory.deploy(debase, dai, burnPool.address);
 
-			let tx = await burnPool.initialize(
+			await burnPool.initialize(
 				debase,
 				oracle.address,
 				policy,
@@ -72,35 +72,30 @@ describe('Debt Issuance Pool', () => {
 				oneDivDeviationSqrtTwoPi,
 				twoDeviationSquare
 			);
-
-			await tx.wait(1);
 		});
 
 		describe('Initial settings check', function() {
 			it('Reward token should be debase', async function() {
 				expect(await burnPool.debase()).eq(debase);
 			});
-			// it('Pair token should be degov lp', async function() {
-			// 	expect(await burnPool.dai()).eq(dai);
-			// });
-			// it('Policy should be policy contract', async function() {
-			// 	expect(await burnPool.policy()).eq(policy);
-			// });
-			// it('Duration should be correct', async function() {
-			// 	expect(await burnPool.oracle()).eq(oracle.address);
-			// });
-			// it('Reward token should be debase', async function() {
-			// 	expect(await burnPool.burnPool1()).eq(burnPool1);
-			// });
-			// it('Pair token should be degov lp', async function() {
-			// 	expect(await burnPool.burnPool2()).eq(burnPool2);
-			// });
-			// it('Policy should be policy contract', async function() {
-			// 	expect(await burnPool.epochs()).eq(epochs);
-			// });
-			// it('Duration should be correct', async function() {
-			// 	expect(await burnPool.oraclePeriod()).eq(oraclePeriod);
-			// });
+			it('Policy should be policy contract', async function() {
+				expect(await burnPool.policy()).eq(policy);
+			});
+			it('Duration should be correct', async function() {
+				expect(await burnPool.oracle()).eq(oracle.address);
+			});
+			it('Reward token should be debase', async function() {
+				expect(await burnPool.burnPool1()).eq(burnPool1);
+			});
+			it('Pair token should be degov lp', async function() {
+				expect(await burnPool.burnPool2()).eq(burnPool2);
+			});
+			it('Policy should be policy contract', async function() {
+				expect(await burnPool.epochs()).eq(epochs);
+			});
+			it('Duration should be correct', async function() {
+				expect(await burnPool.oraclePeriod()).eq(oraclePeriod);
+			});
 		});
 	});
 });
