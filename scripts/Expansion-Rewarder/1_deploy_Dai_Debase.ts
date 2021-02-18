3b4453f4d004bdda5b54b4847b1693e481e52799
PunkUnknown <PunkUnknown@tutanota.com>
2021-02-18 13:35:50 +0500

    new deploy scripts

---
+++ scripts/Expansion-Rewarder/1_deploy_Dai_Debase.ts
@@ -0,0 +1,56 @@
+import { run, ethers } from 'hardhat';
+
+import ExpansionRewarderArtifact from '../../artifacts/contracts/Expansion-Rewarder/ExpansionRewarder.sol/ExpansionRewarder.json';
+import { ExpansionRewarderFactory } from '../../typechain/ExpansionRewarderFactory';
+
+import { parseEther, parseUnits } from 'ethers/lib/utils';
+
+async function main() {
+	const signer = await ethers.getSigners();
+
+	try {
+		const expansionFactory = (new ethers.ContractFactory(
+			ExpansionRewarderArtifact.abi,
+			ExpansionRewarderArtifact.bytecode,
+			signer[0]
+		) as any) as ExpansionRewarderFactory;
+
+		const debase = '0x9248c485b0B80f76DA451f167A8db30F33C70907';
+		const pair = '0xE98f89a2B3AeCDBE2118202826478Eb02434459A';
+		const debasePolicy = '0x989Edd2e87B1706AB25b2E8d9D9480DE3Cc383eD';
+		const rewardPercentage = parseUnits('5', 17);
+		const multiSigAddress = '0xf038C1cfaDAce2C0E5963Ab5C0794B9575e1D2c2';
+		const multiSigShare = parseUnits('2', 17);
+		const stabilityPercentage = parseUnits('5', 17);
+
+		const duration = 25;
+		const userLpLimit = parseEther('20');
+        const userLpEnabled = true
+		const poolLpLimit = parseEther('50');
+        const poolLpEnabled = true
+
+		const expansion = await expansionFactory.deploy(
+			debase,
+			pair,
+			debasePolicy,
+			rewardPercentage,
+			duration,
+			stabilityPercentage,
+			multiSigShare,
+			multiSigAddress,
+			userLpEnabled,
+			userLpLimit,
+			poolLpEnabled,
+			poolLpLimit
+		);
+
+		console.log(expansion.address);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+main().then(() => process.exit(0)).catch((error) => {
+	console.error(error);
+	process.exit(1);
+});
